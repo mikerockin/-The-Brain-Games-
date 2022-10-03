9d7919b7c87c8f3a2c6905c6f527cdf322933186
mikerockin <mrmikhail88@gmail.com>
2022-10-04 02:55:35 +0500

    Added rules_of_games into engine.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -4,8 +4,8 @@ from brain_games.engine import engine
 
 
 def main():
-    engine(prime,
-           'Answer "yes" if given number is prime.'' Otherwise answer "no"')
+    engine(prime, 'Answer "yes" if given number'
+                  ' is prime. Otherwise answer "no".')
 
 
 if __name__ == '__main__':
