5b388f171fc650afe6c774428237966da172f3ca
mikerockin <mrmikhail88@gmail.com>
2022-10-07 09:47:21 +0500

    The rules gets on const, the question moved into the engine

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -12,11 +12,10 @@ def calculates_gcd():
                                    GCD_END_OF_RANGE_RAND_NUMBER_1)
     rand_number_2 = random.randint(GCD_START_OF_RANGE_RAND_NUMBER_2,
                                    GCD_END_OF_RANGE_RAND_NUMBER_2)
-    question = f' {rand_number_1} {rand_number_2} '
+    question = f'{rand_number_1} {rand_number_2}'
     while rand_number_1 != 0 and rand_number_2 != 0:
         if rand_number_1 > rand_number_2:
             rand_number_1 = rand_number_1 % rand_number_2
         else:
             rand_number_2 = rand_number_2 % rand_number_1
-    answer = rand_number_1 + rand_number_2
-    return question, str(answer)
+    return question, str(rand_number_1 + rand_number_2)
